bf1856e79439b6fc5c2ae679ebbaca3f3327b19a
anshori <m.anshory@gmail.com>
2024-10-05 13:26:52 +0800

    Add excalidraw to project react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { Excalidraw } from "@excalidraw/excalidraw";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+				<div style={{ height: "100%" }}>
+					<h2 className="App-title">
+						PAPAN TULIS
+					</h2>
+					<Excalidraw />
+				</div>
+		</div>
+	);
 }
 
 export default App;
